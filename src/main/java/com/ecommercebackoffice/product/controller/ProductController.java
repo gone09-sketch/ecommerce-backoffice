b4fd7f80fda9ba1c9552e8936459e4f42b83b9fc
sirisiyesiri <shil690321@naver.com>
2026-04-24 17:12:13 +0900

    refactor: ProductController 세팅

--- src/main/java/com/ecommercebackoffice/product/controller/ProductController.java
+++ src/main/java/com/ecommercebackoffice/product/controller/ProductController.java
@@ -1,4 +1,12 @@
 package com.ecommercebackoffice.product.controller;
 
+import com.ecommercebackoffice.product.service.ProductService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
 public class ProductController {
+
+    private final ProductService productService;
 }
