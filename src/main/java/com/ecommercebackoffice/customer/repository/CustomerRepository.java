d328ca9e7bdf8a0cdb15090cc3fc992c7c574f00
han-yejin <biziyejin8844@naver.com>
2026-04-24 20:13:24 +0900

    feat: CustomerRepository 추가

--- src/main/java/com/ecommercebackoffice/customer/repository/CustomerRepository.java
+++ src/main/java/com/ecommercebackoffice/customer/repository/CustomerRepository.java
@@ -1,4 +1,10 @@
 package com.ecommercebackoffice.customer.repository;
 
-public class CustomerRepository {
+import com.ecommercebackoffice.customer.entity.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
+
+
+
 }
