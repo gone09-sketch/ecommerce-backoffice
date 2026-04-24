10ac8ee2266087387838e4648971f5b3eb8611e4
sirisiyesiri <shil690321@naver.com>
2026-04-24 17:10:15 +0900

    refactor: ProductService 세팅

--- src/main/java/com/ecommercebackoffice/product/service/ProductService.java
+++ src/main/java/com/ecommercebackoffice/product/service/ProductService.java
@@ -1,4 +1,12 @@
 package com.ecommercebackoffice.product.service;
 
+import com.ecommercebackoffice.product.repository.ProductRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class ProductService {
+
+    private final ProductRepository productRepository;
 }
