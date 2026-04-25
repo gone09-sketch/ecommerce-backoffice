eb8c434213d2cc3e440fbff5a446727e56104d16
Jiwon Yang <g.one9709@gmail.com>
2026-04-25 23:39:16 +0900

    feat: 관리자 등록 API 변경 -AdminCreateResponse from 메서드 추가 -AdminService > 관리자 등록(회원가입) 수정

--- src/main/java/com/ecommercebackoffice/admin/dto/AdminCreateResponse.java
+++ src/main/java/com/ecommercebackoffice/admin/dto/AdminCreateResponse.java
@@ -1,5 +1,6 @@
 package com.ecommercebackoffice.admin.dto;
 
+import com.ecommercebackoffice.admin.entity.Admin;
 import com.ecommercebackoffice.admin.enums.AdminRole;
 import com.ecommercebackoffice.admin.enums.AdminStatus;
 import lombok.Getter;
@@ -13,19 +14,35 @@ public class AdminCreateResponse {
     private final String name;
     private final String email;
     private final String phoneNumber;
-    private final AdminRole role;
-    private final AdminStatus status;
+    private final String role;
+    private final String status;
     private final LocalDateTime createdAt;
+    private final LocalDateTime updatedAt;
 
     // 생성자
     public AdminCreateResponse(Long id, String name, String email, String phoneNumber,
-                              AdminRole role, LocalDateTime createdAt ) {
+                               String role, String status, LocalDateTime createdAt, LocalDateTime updatedAt ) {
         this.id = id;
         this.name = name;
         this.email = email;
         this.phoneNumber = phoneNumber;
         this.role = role;
-        this.status = AdminStatus.PENDING;
+        this.status = status;
         this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+    }
+
+    // 기능
+    public static AdminCreateResponse from(Admin admin) {
+        return new AdminCreateResponse(
+                admin.getId(),
+                admin.getName(),
+                admin.getEmail(),
+                admin.getPhoneNumber(),
+                admin.getRole().name(),
+                admin.getStatus().name(),
+                admin.getCreatedAt(),
+                admin.getUpdatedAt()
+        );
     }
 }
