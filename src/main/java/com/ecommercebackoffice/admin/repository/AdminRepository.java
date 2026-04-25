d69f1dcfc23fc6092b2e875fb3d19c54c5f7848e
Jiwon Yang <g.one9709@gmail.com>
2026-04-25 19:07:56 +0900

    feat: existsByEmail 중복 이메일 메서드 구현(AdminRepository)

--- src/main/java/com/ecommercebackoffice/admin/repository/AdminRepository.java
+++ src/main/java/com/ecommercebackoffice/admin/repository/AdminRepository.java
@@ -5,4 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AdminRepository extends JpaRepository<Admin, Long> {
 
+    // 중복 이메일 확인
+    boolean existsByEmail(String email);
+
 }
