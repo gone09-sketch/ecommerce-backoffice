0cc7dbbf1e698af81ccc53a5e443327cc771410e
Jiwon Yang <g.one9709@gmail.com>
2026-04-26 04:14:59 +0900

    feat: 관리자 상세 조회 응답에 승인일자 및 role & status를 description으로 변경

--- src/main/java/com/ecommercebackoffice/admin/dto/AdminCreateResponse.java
+++ src/main/java/com/ecommercebackoffice/admin/dto/AdminCreateResponse.java
@@ -18,10 +18,11 @@ public class AdminCreateResponse {
     private final String status;
     private final LocalDateTime createdAt;
     private final LocalDateTime updatedAt;
+    private final LocalDateTime approvedAt; // null 허용
 
     // 생성자
-    public AdminCreateResponse(Long id, String name, String email, String phoneNumber,
-                               String role, String status, LocalDateTime createdAt, LocalDateTime updatedAt ) {
+    public AdminCreateResponse(Long id, String name, String email, String phoneNumber, String role, String status,
+                               LocalDateTime createdAt, LocalDateTime updatedAt, LocalDateTime approvedAt) {
         this.id = id;
         this.name = name;
         this.email = email;
@@ -30,6 +31,7 @@ public class AdminCreateResponse {
         this.status = status;
         this.createdAt = createdAt;
         this.updatedAt = updatedAt;
+        this.approvedAt = approvedAt;
     }
 
     // 기능
@@ -39,10 +41,11 @@ public class AdminCreateResponse {
                 admin.getName(),
                 admin.getEmail(),
                 admin.getPhoneNumber(),
-                admin.getRole().name(),
-                admin.getStatus().name(),
+                admin.getRole().getDescription(),
+                admin.getStatus().getDescription(),
                 admin.getCreatedAt(),
-                admin.getUpdatedAt()
+                admin.getUpdatedAt(),
+                admin.getApprovedAt()
         );
     }
 }
