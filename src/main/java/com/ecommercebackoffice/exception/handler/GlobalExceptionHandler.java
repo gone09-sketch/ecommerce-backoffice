14904704ddad1dfaef9d2a6f3dfbe84b24aa3461
Jiwon Yang <g.one9709@gmail.com>
2026-04-25 16:30:30 +0900

    feat: GlobalExceptionHandler 구현

--- src/main/java/com/ecommercebackoffice/exception/handler/GlobalExceptionHandler.java
+++ src/main/java/com/ecommercebackoffice/exception/handler/GlobalExceptionHandler.java
@@ -1,6 +1,31 @@
 package com.ecommercebackoffice.exception.handler;
 
+import com.ecommercebackoffice.exception.ServiceException;
+import com.ecommercebackoffice.exception.dto.ErrorResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+@RestControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(ServiceException.class)
+    public ResponseEntity<ErrorResponse> handlerServiceException(ServiceException e) {
+
+        // 1. 응답 내용 (Body)
+        String message = e.getMessage();
+        ErrorResponse body = new ErrorResponse(message);
+
+        // 2. 상태코드 준비 (Header)
+        HttpStatus status = e.getStatus();
+
+        // 3. ResponseEntity 객체 생성
+        ResponseEntity<ErrorResponse> exceptionResponse = new ResponseEntity<>(body, status);
+
+        // 4. 반환
+        return exceptionResponse;
+
+    }
+
 }
