42713853d301369a67621cd98f31f96fbe050085
Jiwon Yang <g.one9709@gmail.com>
2026-04-27 15:35:48 +0900

    feat: Enum 유효하지 않은 값 예외 처리 추가

--- src/main/java/com/ecommercebackoffice/exception/handler/GlobalExceptionHandler.java
+++ src/main/java/com/ecommercebackoffice/exception/handler/GlobalExceptionHandler.java
@@ -4,6 +4,7 @@ import com.ecommercebackoffice.exception.ServiceException;
 import com.ecommercebackoffice.exception.dto.ErrorResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -25,7 +26,23 @@ public class GlobalExceptionHandler {
 
         // 4. 반환
         return exceptionResponse;
-
     }
 
+    // 요청 본문(JSON)을 읽을 수 없을 때 자동 발생함 (이넘 값이 아닐 때)
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<ErrorResponse> handleHttpMessageNotReadable(HttpMessageNotReadableException e) {
+
+        // 1. 응답 내용 (Body)
+        String message = "유효하지 않은 값입니다.";
+        ErrorResponse body = new ErrorResponse(message);
+
+        // 2. 상태코드 준비 (Header)
+        HttpStatus status = HttpStatus.BAD_REQUEST;
+
+        // 3. ResponseEntity 객체 생성
+        ResponseEntity<ErrorResponse> exceptionResponse = new ResponseEntity<>(body, status);
+
+        // 4. 반환
+        return exceptionResponse;
+    }
 }
