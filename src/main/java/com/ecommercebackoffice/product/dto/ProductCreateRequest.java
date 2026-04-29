d0cff6a746ad7d730d19017c517560840e5cf80b
sirisiyesiri <shil690321@naver.com>
2026-04-29 14:30:57 +0900

    refactor: Validation 수정

--- src/main/java/com/ecommercebackoffice/product/dto/ProductCreateRequest.java
+++ src/main/java/com/ecommercebackoffice/product/dto/ProductCreateRequest.java
@@ -3,6 +3,8 @@ package com.ecommercebackoffice.product.dto;
 import com.ecommercebackoffice.product.enums.ProductStatus;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -14,10 +16,11 @@ public class ProductCreateRequest {
     private String name;
     @NotBlank
     private String category;
-    @NotBlank
+    @NotNull
+    @Positive
     private Long price;
     @Min(1)
     private int stock;
-    @NotBlank
+    @NotNull
     private ProductStatus status;
 }
