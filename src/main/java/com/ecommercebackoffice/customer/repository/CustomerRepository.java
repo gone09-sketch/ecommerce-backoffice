e2e91453396ffd0595a5ac64114eb3999b11cbff
han-yejin <biziyejin8844@naver.com>
2026-04-30 14:28:58 +0900

    fix: 불필요한 매개변수 값 삭제

--- src/main/java/com/ecommercebackoffice/customer/repository/CustomerRepository.java
+++ src/main/java/com/ecommercebackoffice/customer/repository/CustomerRepository.java
@@ -2,8 +2,6 @@ package com.ecommercebackoffice.customer.repository;
 
 import com.ecommercebackoffice.customer.entity.Customer;
 import com.ecommercebackoffice.customer.enums.CustomerStatus;
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -26,5 +24,5 @@ public interface CustomerRepository extends JpaRepository<Customer, Long> {
 
 
 
-    boolean existsByEmail(@NotBlank(message = "이메일은 필수입니다.") @Email(message = "올바른 이메일 형식이어야 합니다.") String email);
+    boolean existsByEmail(String email);
 }
