88cb581a4071dfbf7cd018203ac8f94516b7fedd
rladl <rladlsdks12@gmail.com>
2026-04-30 12:08:50 +0900

    refactor : InvalidAdminStatusException , InvalidCredentialsException : 공통응답으로 수정

--- src/main/java/com/ecommercebackoffice/exception/InvalidAdminStatusException.java
+++ src/main/java/com/ecommercebackoffice/exception/InvalidAdminStatusException.java
@@ -1,7 +1,11 @@
 package com.ecommercebackoffice.exception;
 
-public class InvalidAdminStatusException extends RuntimeException {
+import org.springframework.http.HttpStatus;
+
+public class InvalidAdminStatusException extends ServiceException {
+
+    // 외부(AuthService)에서 던져주는 구체적인 메시지를 받아 부모에게 전달합니다.
     public InvalidAdminStatusException(String message) {
-        super(message);
+        super(HttpStatus.FORBIDDEN, message);
     }
 }
--- src/main/java/com/ecommercebackoffice/session/AuthService.java
+++ src/main/java/com/ecommercebackoffice/session/AuthService.java
@@ -16,18 +16,19 @@ public class AuthService {
     private final PasswordEncoder passwordEncoder;
 
     public Admin authenticate(String email, String password) {
-        // 1. 이메일로 관리자 조회
+        // 1. 이메일로 관리자 조회 (예외 발생 시 괄호 안을 비웁니다)
+
+        // () -> new 클래스 형태보단 클래스: :new 더짧은 문법으로 대체 가능
         Admin admin = adminRepository.findByEmail(email)
-                .orElseThrow(() -> new InvalidCredentialsException("이메일 또는 비밀번호가 잘못되었습니다."));
+                .orElseThrow(InvalidCredentialsException::new);
 
         // 2. 비밀번호 검증
         if (!passwordEncoder.matches(password, admin.getPassword())) {
-            throw new InvalidCredentialsException("이메일 또는 비밀번호가 잘못되었습니다. ");
+            throw new InvalidCredentialsException();
         }
 
-        // 3. 관리자 상태 검증 (ACTIVE 상태만 로그인 허용)
-        // admin.getStatus()가 Enum 타입이라고 가정할 때의 로직입니다.
-        checkAdminStatus(admin);
+        // 3. 관리자 상태 검증
+        checkAdminStatus(admin); // 여기는 외부에서 메세지를 주입하므로 기존 코드 그대로 유지!
 
         return admin;
     }
