ac7b34dacfe6504f1ddcea552e1b85850cb5035f
rladl <rladlsdks12@gmail.com>
2026-04-30 11:24:39 +0900

    refactor :InvalidCredentialsException 불 필요한 주석 삭제

--- src/main/java/com/ecommercebackoffice/exception/InvalidCredentialsException.java
+++ src/main/java/com/ecommercebackoffice/exception/InvalidCredentialsException.java
@@ -1,7 +1,7 @@
 package com.ecommercebackoffice.exception;
 
-public class InvalidCredentialsException extends RuntimeException { // 1. 클래스 여는 중괄호
-    public InvalidCredentialsException(String message) { // 2. 생성자 여는 중괄호
+public class InvalidCredentialsException extends RuntimeException {
+    public InvalidCredentialsException(String message) {
         super(message);
-    } // 3. 생성자 닫는 중괄호 (클래스를 닫는 중괄호가 없음!)
+    }
 }
